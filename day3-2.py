80225315781c27d467d4a89076918d344fab3605
feischl97 <feischl97@gmail.com>
2021-12-12 05:11:20 +0000

    improve algorithm to be 2.5x faster

--- day3-2.py
+++ day3-2.py
@@ -1,3 +1,6 @@
+import time
+
+
 lines = open("input.txt").readlines()
 isOxygen = True
 
@@ -26,20 +29,27 @@ def filter_fun():
     for index in range(12):
         if (len(arr) == 1): return arr[0]
         bit = most_common_at_index(index, arr) if isOxygen else least_common_at_index(index, arr)
-        for line in lines:
+        idx = 0
+        while (idx < len(arr)):
+            line = arr[idx]
             if (line[index] != bit):
                 try:
                     arr.remove(line)
+                    idx -= 1
                 except ValueError:
                     pass
+            idx += 1
     return arr[0]
 
 def main():
+    start = time.time()
     oxygen = filter_fun()
     global isOxygen
     isOxygen = False
     co2 = filter_fun()
+    end = time.time()
     print(int(oxygen, 2) * int(co2, 2))
+    print(end-start)
     
 if __name__ == "__main__":
     main()
